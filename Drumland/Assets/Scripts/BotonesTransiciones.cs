220b71fc64d6aad6c5df25f4fbd818fd294f2030
Rodrigo Mendoza <A01720627@itesm.mx>
2022-04-12 19:33:48 -0500

    Login y registro
    Implementación de las escenas de login y registro ya con la confirmación de la bases de datos a base de código php

---
+++ Drumland/Assets/Scripts/BotonesTransiciones.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Rodrigo Alfredo Mendoza España
+// scripts para cambiar de escenas
+public class BotonesTransiciones : MonoBehaviour
+{
+    //Para aplicarse a un botón que cierra la aplicación
+    public void SalirAplicacion()
+    {
+        Application.Quit();
+    }
+
+    public void LogOff()
+    {
+        SceneManager.LoadScene("Login");
+    }
+}
