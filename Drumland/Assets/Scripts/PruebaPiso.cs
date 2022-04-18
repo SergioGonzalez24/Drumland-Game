4696636f1fb495d5e057783ac6b900987bdcf0e5
YunoeSd <99697763+YunoeSd@users.noreply.github.com>
2022-04-18 18:53:58 -0500

    ActualizacionNivel2

--- Drumland/Assets/Scripts/PruebaPiso.cs
+++ Drumland/Assets/Scripts/PruebaPiso.cs
@@ -8,19 +8,19 @@ using UnityEngine;
  */
 public class PruebaPiso : MonoBehaviour
 {
-    public static bool estaEnPiso = false;
+    public static bool saltando = false;
 
 
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        estaEnPiso = true;
+        saltando = true;
     }
 
     // Update is called once per frame
     private void OnTriggerExit2D(Collider2D collision)
     {
-        estaEnPiso = false;
+        saltando = false;
 
     }
 }
